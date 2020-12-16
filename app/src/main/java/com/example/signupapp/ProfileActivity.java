a433aee12896cdb255002fe9d3a5f11f709c7e38
Hasan Cihad Altay <caltayzzz@gmail.com>
2020-12-16 04:53:45 +0300

    pp ekleme yaptım, firebase'e ekleme yapılacak

--- app/src/main/java/com/example/signupapp/ProfileActivity.java
+++ app/src/main/java/com/example/signupapp/ProfileActivity.java
@@ -1,22 +1,90 @@
 package com.example.signupapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final int CHOOSE_IMAGE = 101;
+    ImageView imageViewChoosePicture;
+    Uri uriProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageViewChoosePicture = findViewById(R.id.imageView);
+
+        imageViewChoosePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showImageChooser();
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uriProfileImage = data.getData();
+            Bitmap bitmap = uriToBitmap(uriProfileImage);
+            imageViewChoosePicture.setImageBitmap(bitmap);
+        }
+    }
+
+    private void showImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Bir resim seçin"), CHOOSE_IMAGE);
+    }
+
+    private Bitmap uriToBitmap(Uri selectedFileUri) {
+        Bitmap bitmap = null;
+        try {
+            ParcelFileDescriptor parcelFileDescriptor =
+                    getContentResolver().openFileDescriptor(selectedFileUri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+
+            parcelFileDescriptor.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return RotateBitmap(bitmap, -90);
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
